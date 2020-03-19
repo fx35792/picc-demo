0c134525cf1077243042fe5377dfb7233e18d8b1
sunnyFan <fx35792@163.com>
2020-03-19 15:14:54 +0800

    input 左侧图片自定义

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import router from './router/index'
 import App from './App.vue'
 import Vant from 'vant';
+import './assets/css/reset.css';
 import 'vant/lib/index.css';
-
+import './assets/font/iconfont.css';
+import './assets/font/iconfont.js'
 
 Vue.config.productionTip = false
 Vue.use(Vant)
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -8,17 +8,31 @@
       bind:click-right="onClickRight"
     />
     <van-form>
-      <van-field v-model="userName" left-icon="user-o" name="用户名" label="用户名" placeholder="用户名" />
-      <!-- :rules="[{ required: true, message: '请填写用户名' }]" -->
-      <van-field
-        v-model="password"
-        left-icon="eye-o"
-        type="password"
-        name="密码"
-        label="密码"
-        placeholder="密码"
-      />
-      <!-- :rules="[{ required: true, message: '请填写密码' }]" -->
+      <!-- 第一种方式：通过 van-icon 的 name和classs-prefix属性  这个地方借助的是iconfont.css文件 -->
+      <van-field v-model="userName" left-icon="user-o" name="用户名" label="用户名" placeholder="用户名">
+        <van-icon slot="left-icon" name="user" class-prefix="iconfont"></van-icon>
+      </van-field>
+      <van-field v-model="password" type="password" name="密码" label="密码" placeholder="密码">
+        <van-icon slot="left-icon" name="password" class-prefix="iconfont"></van-icon>
+      </van-field>
+      <!-- 第一种方式：通过 van-icon 嵌套svg 这个地方借助的是iconfont.js和iconfont.svg文件 -->
+      <van-field v-model="password" type="password" name="密码" label="密码" placeholder="密码">
+        <van-icon slot="left-icon">
+          <svg class="icon icon1" aria-hidden="true">
+            <use xlink:href="#iconfont-password" />
+          </svg>
+        </van-icon>
+      </van-field>
+      <!-- 第三种方式：通过 img 设计图片 -->
+      <van-field v-model="password" type="password" name="密码" label="密码" placeholder="密码">
+        <img
+          slot="left-icon"
+          width="15"
+          height="15"
+          src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1584607677636&di=e538499fa82e9b5d539f6b0118054588&imgtype=0&src=http%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_550783.png"
+          alt
+        />
+      </van-field>
       <div style="margin: 16px;">
         <van-button round block type="info" @click="handleSubmit">提交</van-button>
       </div>
@@ -48,3 +62,8 @@ export default {
   }
 };
 </script>
+<style lang="css" scoped>
+.icon1 {
+  fill: blue;
+}
+</style>
